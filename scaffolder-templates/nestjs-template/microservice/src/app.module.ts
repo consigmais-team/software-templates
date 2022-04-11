4ed7aad93f0e19caaa612896ac62295f7b772c13
Jhonatan Teixeira <jhonatan.teixeira@gmail.com>
2022-04-11 11:02:09 -0300

    melhorias nestjs

--- scaffolder-templates/nestjs-template/microservice/src/app.module.ts
+++ scaffolder-templates/nestjs-template/microservice/src/app.module.ts
@@ -1,11 +1,14 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './controller/user.controller';
 import { User } from './entity/user.entity';
 import { UserService } from './service/user.service';
+import { HealthController } from './controller/health.controller';
 
 @Module({
   imports: [
@@ -20,8 +23,10 @@ import { UserService } from './service/user.service';
       entities: [User],
     }),
     TypeOrmModule.forFeature([User]),
+    HttpModule,
+    TerminusModule,
   ],
-  controllers: [AppController, UserController],
+  controllers: [AppController, UserController, HealthController],
   providers: [AppService, UserService],
 })
 export class AppModule {}
